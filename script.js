af1daa35531c1b02daf54b694e396f69bee5c996
Yagiz Hitit <yagizhitit@gmail.com>
2023-10-25 23:58:11 +0100

    page 2 done

--- script.js
+++ script.js
@@ -64,3 +64,18 @@ gsap.to("#page-bottom", {
   },
   opacity: 0
 })
+
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '#page1',
+    start: 'top top',
+    scrub: 1,
+    scroller: '#main',
+    markers: true,
+    pin: true
+  }
+})
+
+tl.to("#page1>h1", {
+  top: '-50%'
+})
